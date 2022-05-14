65001248ec51b50d98abe22db079173206a17385
Irina Dolgova <claycharm@yandex.ru>
2022-05-14 19:51:41 +0300

    pick up mistakes

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,15 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    void shouldborder() {
+    void shouldBorder() {
         SQRService service = new SQRService();
         int minimalLimit = 200;
         int maximumLimit = 300;
-        int count = 10;
         int expected = 3;
 
         int actual = service.border(minimalLimit, maximumLimit);
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldBorderAnother() {
+        SQRService service = new SQRService();
+        int minimalLimit = 300;
+        int maximumLimit = 400;
+        int expected = 2;
+
+        int actual = service.border(minimalLimit, maximumLimit);
+
+        assertEquals(expected, actual);
     }
+}
